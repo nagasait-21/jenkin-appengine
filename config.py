04dc6e3a0d6a20014bcd0f4a79bb00f4b45b9ce3
nagasai <nagasaitholeti21@gmail.com>
2026-06-22 23:33:59 +0530

    appenginecicd

--- config.py
+++ config.py
@@ -90,6 +90,6 @@ MONGO_URI = 'mongodb://user:password@host:27017/database'
 #
 # You can adjust the max content length and allow extensions settings to allow
 # larger or more varied file types if desired.
-CLOUD_STORAGE_BUCKET = ' jenkin'
+CLOUD_STORAGE_BUCKET = 'jenkin'
 MAX_CONTENT_LENGTH = 8 * 1024 * 1024
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
